fed5d4e80b8c1a52dad1f5cd79e1f7bc6a500c7b
ThallesP <contato@thalles.me>
2021-07-11 21:45:26 -0300

    :art: Mensagens devem ser em portugues
    Mensagens do console mudada para portugues e removido dependencias extras

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import cv2 as cv
 import cvlib
-from cvlib.object_detection import draw_bbox
-import time
-import numpy as np
 
 # Captura a primeira Webcam no computador, você também pode passar um path relativo para um vídeo.
 cap = cv.VideoCapture(0)
@@ -15,13 +12,11 @@ def resizeFrame(frame, scale=0.75):
 
     return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
 
-
-
 while True:
     isThereFrames, frame = cap.read()
 
     if isThereFrames == False:
-        print("No more frames from Video Capture, ending.")
+        print("Sem mais frames, terminando o programa.")
         break
 
     # Redimensiona o frame para 75% para diminuir o uso de recursos e também acelerar a detecção
@@ -37,7 +32,7 @@ while True:
         cv.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0)) 
 
 
-    cv.imshow("Detecting faces...", frame)
+    cv.imshow("Detectando rostos!", frame)
 
     # Se a tecla "D" for pressionada, termine o programa.
     if cv.waitKey(20) & 0xFF == ord("d"): 
