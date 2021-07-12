5b1848fae1cbfc420f6372501f501752bf8e7008
ThallesP <contato@thalles.me>
2021-07-11 21:42:42 -0300

    :tada: Primeiro commit!
    Primeiro commit!

---
+++ README.md
@@ -0,0 +1,21 @@
+<h1 align="center">Detecção de rostos</h1>
+
+Um simples programa em Python usando o OpenCV para detectar os rostos na sua Webcam e mostrar onde estão!
+
+## 💻 Tecnologias usadas
+- [OpenCV](https://opencv.org/)
+- [Python3](https://www.python.org/)
+
+## 🚀 Como executar
+
+- Clone o repositório
+- Rode `pip install opencv` para baixar opencv-python
+- Rode `pip install cvlib` para baixar cvlib
+- Rode `python3 main.py` para executar o programa
+
+Por fim, irá abrir uma janela com sua Webcam mostrando os rostos presentes nela!
+
+
+## 📝 Contato
+
+Se você tiver alguma dúvida ou problema abra uma issue no repositório ou me contate pelo e-mail: contato@thalles.me
---
+++ main.py
@@ -0,0 +1,48 @@
+import cv2 as cv
+import cvlib
+from cvlib.object_detection import draw_bbox
+import time
+import numpy as np
+
+# Captura a primeira Webcam no computador, você também pode passar um path relativo para um vídeo.
+cap = cv.VideoCapture(0)
+
+def resizeFrame(frame, scale=0.75):
+    width = int(frame.shape[1] * scale)
+    height = int(frame.shape[0] * scale)
+
+    dimensions = (width, height)
+
+    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+
+
+
+while True:
+    isThereFrames, frame = cap.read()
+
+    if isThereFrames == False:
+        print("No more frames from Video Capture, ending.")
+        break
+
+    # Redimensiona o frame para 75% para diminuir o uso de recursos e também acelerar a detecção
+    frame = resizeFrame(frame)
+
+    # Detecta os rostos presentes e retornar coordenadas deles.
+    faces, conf = cvlib.detect_face(frame)
+
+    for face in faces:
+        (startX, startY, endX, endY) = face[0], face[1], face[2], face[3]
+
+        # Desenhe um retângulo no frame com a cor verde.
+        cv.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0)) 
+
+
+    cv.imshow("Detecting faces...", frame)
+
+    # Se a tecla "D" for pressionada, termine o programa.
+    if cv.waitKey(20) & 0xFF == ord("d"): 
+        break
+
+cap.release()
+cv.destroyAllWindows()
+cv.waitKey(0)
